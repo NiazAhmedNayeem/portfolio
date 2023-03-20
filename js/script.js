8b6c139a2a2e3e30c0732223fa78119b680cdb8c
NiazAhmedNayeem <niazahmed.net@gmail.com>
2023-03-21 00:46:51 +0600

    update

--- js/script.js
+++ js/script.js
@@ -42,14 +42,3 @@ let calcScrollValue = () => {
   window.onload = calcScrollValue;
 
 
-  const countEl = document.getElementById('count');
-  
-  updateVisitCount();
-
-  function updateVisitCount(){
-    fetch('https://api.countapi.xyz/update/niazahmednayeem.github.io/portfolio/?amount=1')
-        .then(res => res.json())
-        .then(res => {
-          countEl.innerHTML = res.value;
-        });
-  }
